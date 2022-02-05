10ef711afb5510aec513694d1422d45facd8e6ac
Dhananjay <dhananjaymane11@gmail.com>
2022-02-05 19:52:34 +0530

    Added loader

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { DataGrid, GridRowId } from '@mui/x-data-grid';
 import { Dispatch } from "redux";
 
 import "./App.scss";
+import Loader from './components/Loader';
 import { getTodoData } from "./store/actionCreators";
 
 const columns = [
@@ -15,7 +16,7 @@ const columns = [
 
 const App: React.FC = () => {
     const [selectionModel, setSelectionModel] = React.useState<GridRowId[]>([]);
-
+    const [showLoader, setShowLoader] = React.useState<boolean>(true);
     const todoData: readonly ITodo[] = useSelector(
         (state: AppState) => state.todoData,
     );
@@ -26,31 +27,41 @@ const App: React.FC = () => {
         dispatch(getTodoData());
     }, [dispatch]);
 
+    React.useEffect(() => {
+        if (todoData.length && showLoader) {
+            setShowLoader(false)
+        }
+    }, [todoData, showLoader])
+
     const deleteTodoData = (data: GridRowId[]) => dispatch({ type: 'DELETE_DATA', payload: data });
 
     return (
         <main className="main">
+            {
+                showLoader ?
+                    <Loader /> :
+                    <>
+                        <div className="header">
+                            <h1>To Do App</h1>
+                            <Button
+                                variant="outlined"
+                                color="error"
+                                onClick={() => deleteTodoData(selectionModel)}
+                            >Delete</Button>
+                        </div>
 
-            <div className="header">
-                <h1>To Do App</h1>
-                <Button
-                    variant="outlined"
-                    color="error"
-                    onClick={() => deleteTodoData(selectionModel)}
-                >Delete</Button>
-            </div>
-
-            <div style={{ height: 500, width: '100%' }}>
-                <DataGrid
-                    rows={todoData}
-                    columns={columns}
-                    pageSize={25}
-                    checkboxSelection
-                    selectionModel={selectionModel}
-                    onSelectionModelChange={(model) => setSelectionModel(model)}
-                />
-            </div>
-
+                        <div style={{ height: 500, width: '100%' }}>
+                            <DataGrid
+                                rows={todoData}
+                                columns={columns}
+                                pageSize={25}
+                                checkboxSelection
+                                selectionModel={selectionModel}
+                                onSelectionModelChange={(model) => setSelectionModel(model)}
+                            />
+                        </div>
+                    </>
+            }
         </main>
     );
 };
